abdfa81bff4d7d401e9887cfbd210514c34fee94
chamisg <snoblefuentes@gmail.com>
2024-07-03 17:52:41 +0400

    init project and defined auth apis

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\API\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+ 
+    return ['token' => $token->plainTextToken];
+});
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+        
+Route::middleware('auth:sanctum')->group( function () {
+    // Route::resource('user', ProductController::class);
+});
